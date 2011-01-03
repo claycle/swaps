ddabda93c73d66eb00387db85749c302ab499089
Clay Luther <claycle@gmail.com>
2011-01-03 14:52:29 -0600

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swaps::Application.config.secret_token = '114773127dd5c545373f9c4bb8edd1c4b4cff2c657918a0899a1ab490d69ce67c6d7e046c91cd5f97a4e3997389a08cdf234ea1927be080d6f6bc30d276b1a3d'
